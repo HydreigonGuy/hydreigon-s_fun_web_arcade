27434acd7564058a8517e3906394bb9ff2a15f0a
HydreigonGuy <ad.thibault18@gmail.com>
2021-12-24 23:01:28 +0100

    Runner: added player, jumping, collisions and game over

--- src/assets/js/runner/draw_functions.js
+++ src/assets/js/runner/draw_functions.js
@@ -4,8 +4,8 @@ function fillBackground(ctx) {
     ctx.fillRect(0, 0, 600, 400);
 }
 
-function displayText(text, plot, ctx) {
-    ctx.font = "30px Arial";
+function displayText(text, plot, ctx, size) {
+    ctx.font = `${size}px Arial`;
     ctx.strokeText(text, plot.x, plot.y);
 }
 
@@ -19,17 +19,30 @@ function fillObjects(objects, ctx) {
     }
 }
 
-function drawGamePending(canvas) {
+function drawGamePending(canvas, game_over, score) {
     let ctx = canvas.getContext("2d");
 
     fillBackground(ctx);
-    displayText("Press SPACE to start!", {'x':10, 'y':'370'}, ctx)
+    displayText("Press SPACE to start!", {'x':10, 'y':370}, ctx, 30);
+    if (game_over) {
+        displayText(`Game Over!`, {'x':10, 'y':70}, ctx, 60);
+        displayText(`Score: ${score}`, {'x':10, 'y':120}, ctx, 20);
+    }
 }
 
-function displayGamePlaying(objects, canvas) {
+function drawPlayer(player, ctx) {
+    ctx.beginPath();
+    ctx.arc(100, player.y, 20, 0, 2 * Math.PI);
+    ctx.stroke();
+    ctx.fillStyle = "black";
+    ctx.fill();
+}
+
+function displayGamePlaying(player, objects, canvas) {
     let ctx = canvas.getContext("2d");
 
     fillBackground(ctx);
     fillObjects(objects, ctx);
     ctx.stroke();
+    drawPlayer(player, ctx);
 }
--- src/assets/js/runner/main.js
+++ src/assets/js/runner/main.js
@@ -3,12 +3,27 @@ var canvas = document.getElementById("game");
 var gamePaused = 1;
 var last_time = 0;
 var objects = [];
+var is_jumping = false;
+var airtime = 0;
+var game_over = false;
+var score = 0;
+
+var player = {
+    'y':375
+};
 
 function handleSpacePress() {
     if (gamePaused == 1) {
         gamePaused = 0;
+        game_over = false;
         return;
     }
+    if (is_jumping) {
+    //    airtime += 100;
+        return;
+    }
+    is_jumping = true;
+    airtime = 100;
 }
 
 document.body.onkeydown = function (e) {
@@ -17,14 +32,14 @@ document.body.onkeydown = function (e) {
 }
 
 function addObjets(time) {
-    if (objects.length == 0 || objects[objects.length - 1].x < 450)
-        if (Math.random() < time * 0.01)
+    if (objects.length == 0 || objects[objects.length - 1].x < 350)
+        if (Math.random() < time * 0.001)
             objects.push({'x':600, 'height':50 + 150 * Math.random()});
 }
 
 function moveObjects(time) {
     for (let i = 0; i < objects.length; i++)
-        objects[i].x -= time * 0.1;
+        objects[i].x -= time * 0.2;
 }
 
 function removeExtraObjects() {
@@ -36,16 +51,56 @@ function removeExtraObjects() {
     }
 }
 
+function makePlayerJump(time) {
+    if (airtime == 0) {
+        player.y += time * 0.6;
+        if (player.y >= 375) {
+            player.y = 375;
+            is_jumping = false;
+        }
+    } else {
+        player.y -= time * 0.6;
+        if (player.y <= 100) {
+            player.y = 100;
+            airtime -= time * 0.6;
+            if (airtime < 0)
+                airtime = 0;
+        }
+    }
+}
+
+function endGame() {
+    gamePaused = 1;
+    is_jumping = false;
+    game_over = true;
+    score = Math.round(last_time / 100);
+    last_time = 0;
+    player.y = 375;
+    objects = [];
+}
+
+function checkForCollision() {
+    for (let i = 0; i < objects.length; i++) {
+        if (objects[i].x >= 80 && objects[i].x <= 120 && 400 - objects[i].height < player.y)
+            return (endGame());
+        if ((objects[i].x - 100) * (objects[i].x - 100) + (400 - objects[i].height - player.y) * (400 - objects[i].height - player.y) <= 400)
+            return (endGame());
+    }
+}
+
 function gameLoop(time) {
     let elapsed_time = time - last_time
 
     if (gamePaused)
-        drawGamePending(canvas);
+        drawGamePending(canvas, game_over, score);
     else {
+        if (is_jumping)
+            makePlayerJump(elapsed_time);
         addObjets(elapsed_time);
         moveObjects(elapsed_time);
         removeExtraObjects();
-        displayGamePlaying(objects, canvas);
+        checkForCollision();
+        displayGamePlaying(player, objects, canvas);
     }
     last_time = time;
     window.requestAnimationFrame(gameLoop);
